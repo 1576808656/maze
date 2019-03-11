6e458a2325b69fea38a01a7097f10db11bd55468
1576808656 <41664059+1576808656@users.noreply.github.com>
2019-03-11 10:00:31 +0800

    Add files via upload

--- AlgoVisualizer.java
+++ AlgoVisualizer.java
@@ -1,15 +1,21 @@
 package mazeProduce;
 import java.awt.EventQueue;
 
+import mazeSolve.LinkedListQueue;
+import mazeSolve.Position;
+import mazeSolve.Queue;
+
 public class AlgoVisualizer {
 	private static int blockSide = 5;
-	private static final int d[][] = {{-1,0},{0,1},{1,0},{0,-1}};
+	private static final int d[][] = {{1,0},{0,1},{-1,0},{0,-1}};
 	private MazeData data;
 	private AlgoFrame frame;
 	private static int DELAY = 3;
+	private SearchRoad road;
 	int position;
 	public AlgoVisualizer(int N,int M) {
 		data = new MazeData(N,M);
+		road = new SearchRoad(N,M);
 		int sceneWidth = data.getM()*blockSide;
 		int sceneHeight = data.getN()*blockSide;
 		
@@ -24,26 +30,45 @@ public class AlgoVisualizer {
 	}
 	
 	public void run() {
-		go(1,1);
+	//	Stack<Position>stack = new ArrayStack<>();
+		Queue<Position> queue = new LinkedListQueue<>();
+		Position p = new Position(data.getEntranceX(),data.getEntranceY()+1);
+		queue.enqueue(p);
+		data.visited[p.getX()][p.getY()] = true;
+		
+		while(queue.getSize() != 0) {
+			Position position = queue.dequeue();
+			for(int i=0;i<4;i++) {
+				int newX = position.getX()+d[i][0]*2;
+				int newY = position.getY()+d[i][1]*2;
+				if(data.inArea(newX, newY) && !data.visited[newX][newY]) {
+					queue.enqueue(new Position(newX,newY,position));
+					data.visited[newX][newY] = true;
+					setData(position.getX()+d[i][0],position.getY()+d[i][1]);
+				}
+				
+			}
+		}
 		setData(-1,-1);
 	}
 	
-	private void go(int x,int y) {
-		
-		if(!data.inArea(x, y))
-			throw new IllegalArgumentException("x and y are out of index in go function");
+//	private void go(int x,int y) {
 
-		data.visited[x][y] = true;
-		for(int i=0;i<4;i++) {
-			int newX = x+d[i][0]*2;
-			int newY = y+d[i][1]*2;
-			if(data.inArea(newX, newY) && !data.visited[newX][newY]) {
-				setData(x+d[i][0],y+d[i][1]);
-				go(newX,newY);
-			}
-				
-		}
-	}
+		//递归生成迷宫算法
+//		if(!data.inArea(x, y))
+//			throw new IllegalArgumentException("x and y are out of index in go function");
+//
+//		data.visited[x][y] = true;
+//		for(int i=0;i<4;i++) {
+//			int newX = x+d[i][0]*2;
+//			int newY = y+d[i][1]*2;
+//			if(data.inArea(newX, newY) && !data.visited[newX][newY]) {
+//				setData(x+d[i][0],y+d[i][1]);
+//				go(newX,newY);
+//			}
+//				
+//		}
+//	}
 	//有时会在数据变动之后才会渲染，因此把渲染代码放在方法里方便调用
 	private void setData(int x,int y) {
 		if(data.inArea(x, y))
---
+++ SearchRoad.java
@@ -0,0 +1,8 @@
+package mazeProduce;
+
+public class SearchRoad {
+	public boolean road[][];
+	public SearchRoad(int N,int M) {
+		road = new boolean[N][M];
+	}
+}
